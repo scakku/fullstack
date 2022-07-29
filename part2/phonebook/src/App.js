0853eedfee2732eeadbfa9ed17250f619d49c2ad
Saku Ylitalo <sakuylitalo@Sakus-MacBook-Pro.local>
2022-07-29 19:50:03 +0300

    phonebook step6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,13 +1,22 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import Persons from "./components/Persons";
 import PersonForm from "./components/PersonForm";
 import Filter from "./components/Filter";
 
-const App = (props) => {
-  const [persons, setPersons] = useState(props.persons);
+const App = () => {
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNumber] = useState("");
   const [filterList, setFilter] = useState("");
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      console.log("promse fulfilled");
+      setPersons(response.data);
+    });
+  }, []);
+  // console.log("render", persons.length, "persons");
 
   const addPerson = (e) => {
     e.preventDefault();
--- part2/phonebook/src/index.js
+++ part2/phonebook/src/index.js
@@ -2,13 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-const persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-  },
-];
+//const persons = [
+//  {
+//    id: 1,
+//    name: "Arto Hellas",
+//  },
+//];
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <App persons={persons} />
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
